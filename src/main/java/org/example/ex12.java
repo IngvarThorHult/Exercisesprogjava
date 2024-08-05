b41516e56b2796b6043ea763be00be8850179fc8
RC_Student_lab <st10434704@rcconnect.edu.za>
2024-08-05 10:45:13 +0000

    prog

---
+++ src/main/java/org/example/ex12.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class ex12 {
+
+    /** Method getRandom returns a random number between 1 and 54,
+     *   excluding the numbers passed in the argument.*/
+    public static int getRandom(int... numbers) {
+        int num;	// Random number
+        boolean isExcluded; // Is the number excluded
+        do {
+            isExcluded = false;
+            num = 1 + (int)(Math.random() * 5);
+            for (int e: numbers) {
+                if (num == e)
+                    isExcluded = true;
+            }
+        } while (isExcluded); // Test if number is Excluded
+        return num;
+    }
+}
